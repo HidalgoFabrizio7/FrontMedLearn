465afb5d2d325abc30058fc7eb5b2091e96594b8
Sebastian <u202015200@upc.edu.pe>
2024-11-03 19:37:37 -0500

    Treatments implementacion

---
+++ src/app/models/treatments.ts
@@ -0,0 +1,7 @@
+export class Treatments{
+  idTreatments: number=0
+  descriptionTreatment: string=""
+  durationTreatment: number=0
+  startDayTreatment:Date = new Date(Date.now())
+  finishDayTreatment:Date = new Date(Date.now())
+}
