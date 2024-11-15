674e8011414acba49b1185c03ae697db92342f19
Andre Zapata <134071744+MrTurry@users.noreply.github.com>
2024-11-15 16:03:32 -0500

    Alimentos y ejercicios

--- src/app/components/diet/listardt/listardt.component.ts
+++ src/app/components/diet/listardt/listardt.component.ts
@@ -2,20 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 import { Diet } from '../../../models/Diet';
 import { DietService } from '../../../services/diet.service';
+import { MatDividerModule } from '@angular/material/divider';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listardt',
   standalone: true,
-  imports: [MatTableModule, MatIconModule,RouterModule],
+  imports: [MatTableModule, MatIconModule,RouterModule, MatCardModule, MatDividerModule,CommonModule],
   templateUrl: './listardt.component.html',
   styleUrl: './listardt.component.css'
 })
 export class ListardtComponent implements OnInit{
   datasource: MatTableDataSource<Diet> = new MatTableDataSource();
-  displayedColumns: string[]=['c1', 'c2', 'c3', 'c4', 'c5','c6','c7','accion01','accion02']
   constructor(private dT: DietService) {}
+
   ngOnInit(): void {
     this.dT.list().subscribe((data)=>{
       this.datasource=new MatTableDataSource(data)
@@ -24,6 +27,7 @@ export class ListardtComponent implements OnInit{
       this.datasource=new MatTableDataSource(data)
     });
   }
+
   delete(id:number){
     this.dT.delete(id).subscribe((data)=> {
       this.dT.list().subscribe((data)=> {
@@ -31,4 +35,49 @@ export class ListardtComponent implements OnInit{
       });
     });
   }
-}
+  
+  startDiet(diet: Diet): void {
+    diet.startDayDiet = new Date(); // Captura la fecha actual al hacer clic
+    this.dT.update(diet).subscribe(() => {
+      console.log(`Dieta con ID ${diet.idDiet} inició el ${diet.startDayDiet}`);
+      this.dT.list().subscribe((data) => {
+        this.datasource = new MatTableDataSource(data);
+      });
+    });
+  }
+
+  finishDiet(diet: Diet): void {
+    diet.finishDayDiet = new Date(); // Captura la fecha actual al hacer clic
+    this.dT.update(diet).subscribe(() => {
+      console.log(`Dieta con ID ${diet.idDiet} finalizó el ${diet.finishDayDiet}`);
+      this.dT.list().subscribe((data) => {
+        this.datasource = new MatTableDataSource(data);
+      });
+    });
+  }
+
+  addLike(diet: Diet): void {
+    diet.qualificationDiet += 1;
+    this.dT.update(diet).subscribe((data) => {
+        console.log(`Dieta con ID ${diet.idDiet} recibió un like.`);
+        this.dT.list().subscribe((data) => {
+          this.datasource = new MatTableDataSource(data);
+        });
+      });
+    }
+
+    addDislike(diet: Diet): void {
+      if (diet.qualificationDiet > 0) {
+        diet.qualificationDiet -= 1;
+        this.dT.update(diet).subscribe((data) => {
+          console.log(`Dieta con ID ${diet.idDiet} recibió un dislike.`);
+          this.dT.list().subscribe((data) => {
+            this.datasource = new MatTableDataSource(data);
+          });
+        });
+      }
+    }
+
+    
+  }
+
--- src/app/models/Diet.ts
+++ src/app/models/Diet.ts
@@ -1,4 +1,5 @@
 import { Illness } from "./Illness"
+import { Users } from "./Users"
 
 export class Diet{
     idDiet: number=0
@@ -8,4 +9,5 @@ export class Diet{
     startDayDiet:Date = new Date(Date.now())
     finishDayDiet:Date = new Date(Date.now())
     illness:Illness=new Illness()
+    user:Users=new Users()
 }
